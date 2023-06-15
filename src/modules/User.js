56bf9fb43932af82f063d67b5fcead3bbafcd2ef
soucannabisdev <soucannabisadm@gmail.com>
2023-06-15 20:29:30 -0300

    test

--- src/modules/User.js
+++ src/modules/User.js
@@ -1,6 +1,6 @@
 import directusRequest from './directusRequest'
 
-async function User(){
+async function User(){//
   console.log(localStorage.getItem('user_code'))
   const userCode = localStorage.getItem('user_code')
   const userData = await directusRequest("/items/Users?filter[user_code][_eq]=" + userCode + "&", '', "GET")
