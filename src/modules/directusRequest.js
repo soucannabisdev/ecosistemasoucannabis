2c848f35c396e79515801b0c6a3f31a4e70471f3
soucannabisdev <soucannabisadm@gmail.com>
2023-07-07 20:43:14 -0300

    test build

--- src/modules/directusRequest.js
+++ src/modules/directusRequest.js
@@ -7,9 +7,10 @@ let config = {
   headers:{
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Headers':'Content-Type,Authorization,Access-Control-Allow-Origin,Access-Control-Allow-Headers',
+    'Authorization':process.env.REACT_APP_DIRECTUS_API_TOKEN
   },
   maxBodyLength: Infinity,
-  url: process.env.REACT_APP_DIRECTUS_API_URL+query+'?acess_token='+process.env.REACT_APP_DIRECTUS_API_TOKEN,
+  url: process.env.REACT_APP_DIRECTUS_API_URL+query,
   data: data
 };
 
