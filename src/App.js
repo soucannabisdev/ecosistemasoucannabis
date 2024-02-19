3afb57b080dd5533caac628a76750f7c056cd218
SouCannabis <118570727+soucannabisdev@users.noreply.github.com>
2024-02-19 14:30:15 -0300

    Revert "dsf"
    This reverts commit e7f11a5024d891f604fc673a4d2d2f57a17bb629.

--- src/App.js
+++ src/App.js
@@ -21,7 +21,6 @@ import LostPass from "./components/pages/lost-password";
 import User from "./modules/User";
 import Products from "./components/pages/shop/products";
 import "./styles/general.css";
-import backgroundImage from './images/background.jpg'
 import { Dropdown } from 'react-bootstrap';
 
 function App() {
@@ -69,7 +68,7 @@ function App() {
   return (
     <Router>
       {hiddenLogin && (
-        <div>
+        <div >
           <Routes>
             <Route path="/iniciar-cadastro" element={<SignupEmail />} />
           </Routes>
